ddd8e14c94fc83bdc27ef8a5d99d42f9d60100fb
Milzam Hibatullah <hmilzam@gmail.com>
2024-11-05 14:24:41 +0700

    third: learn about basic inputs in flutter

---
+++ input.py
@@ -0,0 +1,16 @@
+#we try to learn print value based on input
+#first for accepting input we should use function input() as default from python
+# name = input("What is your name? ")
+# #code above means we create variable name and the value is from function input
+# #lets start to print
+# print(name)
+#
+# #lets concate the input with outher string
+# sentence = "Welcome "+name
+# print(sentence)
+
+
+#lets start to ask person name and their favorite foods
+name = input("Enter your name: ")
+favorite_food = input("Enter your favorite food: ")
+print('favorite food of '+ name + ' is ' + favorite_food)
