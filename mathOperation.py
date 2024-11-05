b95bfc5f2edd1219697a90fb2e6811cb488674f0
Milzam Hibatullah <hmilzam@gmail.com>
2024-11-05 15:48:53 +0700

    learn about math operate

---
+++ mathOperation.py
@@ -0,0 +1,25 @@
+#learn about math operate
+# we will implement +. - ,/,//,*,**,%
+
+#define variable
+x = 10
+y = 5
+#plus
+print(x+y)
+#minus
+print(x-y)
+#times
+print(x*y)
+#square
+print(x**y)
+#divide with float
+print(x/y)
+#dive with int
+print(x//y)
+#module
+print(x%y)
+
+# x=x+5
+# print(x)
+x+=5
+print(x)
