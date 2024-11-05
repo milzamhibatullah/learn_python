88708ccd248633145455a3c82938696f3557e218
Milzam Hibatullah <hmilzam@gmail.com>
2024-11-05 16:01:41 +0700

    learn about to round,ceil,floor

--- mathOperation.py
+++ mathOperation.py
@@ -21,5 +21,16 @@ print(x%y)
 
 # x=x+5
 # print(x)
-x+=5
-print(x)
+# x+=5
+# print(x)
+
+#lets learn some math operate function
+#round is to make value to nearest number
+d= 10.5
+print(round(d))
+
+import  math
+#ceil to force value to nearest upper number
+print(math.ceil(d))
+#floor to force value to nearest lower number
+print(math.floor(d))
