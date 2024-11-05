f5d7041502e518d5909c7eee75d67020351faa1c
Milzam Hibatullah <hmilzam@gmail.com>
2024-11-05 13:56:00 +0700

    second: print the value

--- 1stPython.py
+++ 1stPython.py
@@ -1,1 +1,8 @@
+# for function print not only accept string data type, that also accepting numeric
 print('Hello, python')
+print(300)
+
+#what if combine string and numeric like code below
+print('Python '*3)
+#the result is Python printed for 3 times
+#we called it also is expression because the string is operated by 3
