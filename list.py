ef8ba2f3bf1dc3b439680d66f3c2bd487a3e6589
Milzam Hibatullah <hmilzam@gmail.com>
2024-11-06 20:52:13 +0700

    learn about array and metod

---
+++ list.py
@@ -0,0 +1,24 @@
+words = ['hello', 'jams','dev','here']
+print(words)
+
+print(words[0])
+print(words[1:])
+print(words[1:2])
+print(words[-1])
+words.reverse()
+print(words)
+
+for name in words:
+    print('Name: '+name)
+
+words.append(10)
+print(words)
+words.append('HIihi')
+print(words)
+words.remove(10)
+print(words)
+
+for name in words:
+    if name.lower()=='hiihi' or name.lower()=='jams':
+        words.remove(name)
+        print(words)
