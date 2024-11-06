3fd7138fe3235aec44ca8a6737f883ac3dd683c0
Milzam Hibatullah <hmilzam@gmail.com>
2024-11-06 20:10:24 +0700

    for loop

---
+++ forLoop.py
@@ -0,0 +1,11 @@
+name = 'jams'
+
+for character in name:
+    print(character)
+
+# numbers=[1,2,3,4,5]
+# for number in numbers:
+#     print(number*2)
+
+for number in range(2,11,4):
+    print(number)
